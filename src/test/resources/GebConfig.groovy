33ee6eefb05a5057ef8b0597ef3d812af1a5ad91
Marianne I. Tromp <marianne.i.tromp@accenture.com>
2019-04-22 15:14:18 -0400

    Use WebDriverManager to determine appropriate chromedriver version

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,5 +1,8 @@
-import io.github.bonigarcia.wdm.ChromeDriverManager
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+
 import org.openqa.selenium.chrome.ChromeDriver
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 // Location where Geb saves the screenshots and HTML dumps at the end of each test
 reportsDir = 'build/test-reports'
@@ -8,8 +11,7 @@ atCheckWaiting = true
 
 // Run tests in Chrome by default
 driver = {
-    // Download and configure Marionette using https://github.com/bonigarcia/webdrivermanager
-    ChromeDriverManager.getInstance().setup()
+    WebDriverManager.getInstance(CHROME).setup();
 
     new ChromeDriver()
 }
